e05b5f930209194ce11a594c3dda13e6cff061c5
mawshin <mawshin@hotmail.sg>
2018-01-21 22:50:39 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <Health/>
+        <GameOptions/>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+import Health from './components/Health';
+import GameOptions from './components/GameOptions';
+
+export default {
+    name: 'App',
+    components: {
+        GameOptions,
+        Health,
+    }
+};
+
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/Health.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="c-health">
+        <div class="c-health--player">
+            <div class="c-health--name">Player</div>
+            <div class="c-health--bar">100%</div>
+        </div>
+        <div class="c-health--dragon">
+            <div class="c-health--name">Dragon</div>
+            <div class="c-health--bar">100%</div>
+        </div>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+    name: 'Health',
+    data() {
+        return {
+            test: 'Welcome to Your Vue.js App',
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .c-health--player {
+        font-weight: bold;
+        font-size: 18px;
+    }
+</style>
