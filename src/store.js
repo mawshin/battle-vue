017f28840b38751867fac35291bd754d555b502f
Maw Shin Koh <mawshin@hotmail.sg>
2018-01-23 22:51:51 +0800

    store data

---
+++ src/store.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+let store = new Vuex.Store({
+    state: {
+        damage: [],
+        bossLife: 100,
+        playerLife: 100,
+        bossDamage: [],
+    }/*,
+    mutations: {
+        increment (state) {
+            return state.damage
+        }
+    }*/
+});
+
+export default store
